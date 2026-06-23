300fd5cc384dbd7d5151a557dfbdd2ea8ef5ae76
KhanhVD <khsanhansi@gmail.com>
2026-06-23 19:31:22 +0700

    update event handling

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,9 @@
 const submit = () => {
   alert('Form submitted')
 }
-
-const sayHi1 = () => {
-  alert('say hi 1')
-}
-
-const sayHi2 = () => {
-  alert('say hi 2')
-}
 </script>
 
 <template>
-  <button @click.self="sayHi1">
-    <button @click.self="sayHi2">say hi 2</button>
-    say hi 1
-  </button>
-  <form @submit.prevent="submit">
-    <button type="submit">Submit</button>
-  </form>
+  <input @keyup.enter.alt="submit" placeholder="Nhập tên của bạn" />
+  <button @click.shift="submit">Submit</button>
 </template>
