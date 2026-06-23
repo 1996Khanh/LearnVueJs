f4368e541e360d1d87cacafee6da509241d00966
KhanhVD <khsanhansi@gmail.com>
2026-06-23 16:49:23 +0700

    Binding class

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <script setup>
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
-const firstName = ref('Nguyên Văn')
-const lastName = ref('A')
+const isActive = ref(true)
+const hasError = ref(true)
 
-const fullName = computed({
-  get() {
-    return firstName.value + ' ' + lastName.value
-  },
-  set(newValue) {
-    ;[firstName.value, lastName.value] = newValue.split(',')
-  },
-})
+const changeActive = () => {
+  isActive.value = !isActive.value
+}
 
-const changeFullname = () => {
-  fullName.value = 'Nguyễn Văn,B'
+const changeHasError = () => {
+  hasError.value = !hasError.value
 }
 </script>
 
 <template>
-  <div>
-    <p id="count">Fullname: {{ fullName }}</p>
-    <p id="count">First name: {{ firstName }}</p>
-    <p id="count">Last name: {{ lastName }}</p>
-    <button @click="changeFullname">Change fullname</button>
-  </div>
+  <button @click="changeActive" class="button" :class="{ active: isActive }">
+    {{ isActive ? 'Active' : 'Deactive' }}
+  </button>
+
+  <button @click="changeHasError" class="button" :class="{ 'has-error': hasError }">
+    {{ hasError ? 'Has Error' : 'No Error' }}
+  </button>
 </template>
+
+<style>
+.button {
+  border: none;
+  background: #ede9fe;
+  color: #8b5cf6;
+  padding: 10px 14px;
+  border-radius: 6px;
+  cursor: pointer;
+  &:hover {
+    background: #c7d2fe;
+  }
+}
+
+.active {
+  background: #8b5cf6;
+  color: white;
+  &:hover {
+    background: #818cf8;
+  }
+}
+
+.has-error {
+  background: #ef4444;
+  color: #fee2e2;
+  &:hover {
+    background: #f87171;
+  }
+}
+</style>
