f04ca605e7ba03278dfbac3d6142ef7a7688a035
KhanhVD <khsanhansi@gmail.com>
2026-06-23 21:10:44 +0700

    onMounted and onUnMouted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
-<script setup>
-import { ref } from 'vue'
-
-const text = ref('')
-</script>
-
 <template>
-  <p>Text: {{ text }}</p>
-  <input type="text" v-model.lazy="text" />
-  <p>Text: {{ text }}</p>
-  <input type="text" v-model.number="text" />
-  <p>Text: {{ text }}</p>
-  <input type="text" v-model.strim="text" />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>Timer: {{ time }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue'
+
+const time = ref(0)
+const intervalId = ref(null)
+
+onMounted(() => {
+  intervalId.value = setInterval(() => {
+    time.value++
+  }, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId.value)
+})
+</script>
