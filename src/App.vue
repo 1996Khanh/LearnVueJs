ad13cd3879b8c429eb3a075f9b7137c2858b1882
KhanhVD <khsanhansi@gmail.com>
2026-06-23 18:42:28 +0700

    event modifier

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <script setup>
-import { ref, reactive } from 'vue'
-const newTodo = ref('')
-const todos = reactive([
-  {
-    id: 1,
-    text: 'Learn vuejs',
-  },
-  {
-    id: 2,
-    text: 'Build a todo app',
-  },
-])
+const submit = () => {
+  alert('Form submitted')
+}
 
-const addTodo = () => {
-  if (newTodo.value.trim() === '') return
-  const todo = {
-    id: todos.length + 1,
-    text: newTodo.value,
-  }
-  todos.push(todo)
-  newTodo.value = ''
+const sayHi1 = () => {
+  alert('say hi 1')
+}
+
+const sayHi2 = () => {
+  alert('say hi 2')
 }
 </script>
 
 <template>
-  <div>
-    <input type="text" v-model="newTodo" @keyup.enter="addTodo" />
-    <button @click="addTodo">Add todo</button>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.text }}
-      </li>
-    </ul>
-  </div>
+  <button @click.self="sayHi1">
+    <button @click.self="sayHi2">say hi 2</button>
+    say hi 1
+  </button>
+  <form @submit.prevent="submit">
+    <button type="submit">Submit</button>
+  </form>
 </template>
