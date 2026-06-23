bb2164dea2a7cc4baaf5eef981bcbe2e431cf81c
KhanhVD <khsanhansi@gmail.com>
2026-06-23 11:57:42 +0700

    Binding VueJs basic

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+const message = 'Hello world'
+
+const rawHtml = '<div><h1>Title</h1><span style="color: red">This should be red.</span></div>'
+
+const buttonId = 'btnId'
+const buttonClass = 'btnClass'
+const isDisabled = false
+
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: {
+    color: 'blue',
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>Syntax</h1>
+  <p>Message: {{ message }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <span v-html="rawHtml"></span>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <button v-bind:id="buttonId">Click</button>
+  <br />
+  <button :id="buttonId" :class="buttonClass" :disabled="isDisabled">Click</button>
+  <br />
+  <button v-bind="objectOfAttrs">Click here!!!</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
