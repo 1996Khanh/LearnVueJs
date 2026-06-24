bf5cba5fa969bdddd8b970335765d1053e6d5173
KhanhVD <khsanhansi@gmail.com>
2026-06-24 08:39:31 +0700

    static props

---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  init: Number,
+})
+
+const count = ref(props.init)
+</script>
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
