846da2e5f5560026385be6712b78773c07f35399
KhanhVD <khsanhansi@gmail.com>
2026-06-25 22:10:12 +0700

    component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import ButtonCount from './components/ButtonCount.vue'
-import { ref } from 'vue'
-import ChildrenComponent from './components/ChildrenComponent.vue'
-
-const message = ref('Hello')
-const sayHi = () => {
-  console.log('Say hi')
-}
 </script>
 
 <template>
@@ -16,18 +9,8 @@ const sayHi = () => {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld :greeting-message="message + 'World'" />
-      <!-- <button-count :init="2" /> -->
-      <br />
-      <ChildrenComponent
-        :prop-a="12"
-        prop-b="2"
-        prop-c="hello"
-        :prop-d="20"
-        :prop-e="{ message: 'Hello' }"
-        prop-f="success"
-        :prop-g="sayHi"
-      />
+      <HelloWorld msg="You did it!" />
+      <button-count />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/components/ButtonCount.vue
+++ src/components/ButtonCount.vue
@@ -1,12 +1,9 @@
-<script setup>
-import { ref } from 'vue'
-
-const props = defineProps({
-  init: Number,
-})
-
-const count = ref(props.init)
-</script>
 <template>
   <button @click="count++">{{ count }}</button>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
